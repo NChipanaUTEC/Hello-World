ce52e666a9b963d48ec1daa376345160b2ebed04
Nicolás <nicolas.chipana@utec.edu.pe>
2019-03-28 12:22:36 -0500

    Contar Vocales agregado

--- ContarLetras.cpp
+++ ContarLetras.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <set>
 using namespace std;
 
 int main(){
   string texto;
   getline(cin,texto);
   map<char,int> contador;
+  set<char> vocales;
+  vocales.emplace('a');
+  vocales.emplace('e');
+  vocales.emplace('i');
+  vocales.emplace('o');
+  vocales.emplace('u');
   for(int i = 0; i < texto.size(); i++){
-    if(contador.find(texto[i]) == contador.end())
-      contador.emplace(make_pair(texto[i],1));
-    else
-      contador[texto[i]] += 1;
+    if(vocales.find(texto[i])!= vocales.end()){
+      if(contador.find(texto[i]) == contador.end())
+        contador.emplace(make_pair(texto[i],1));
+      else
+        contador[texto[i]] += 1;
+    }
   }
   for(auto letra: contador){
     cout << letra.first << ":"<<letra.second<<endl;
